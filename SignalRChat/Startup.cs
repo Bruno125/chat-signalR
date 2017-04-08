a795d8af7a0a3dc75d7a5980e8962a370025004c
Bruno125 <miguel_bruno@hotmail.com>
2017-04-08 17:36:23 -0500

    Add custom route

--- SignalRChat/Startup.cs
+++ SignalRChat/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using Microsoft.AspNet.SignalR;
 [assembly: OwinStartup(typeof(SignalRChat.Startup))]
 namespace SignalRChat
 {
@@ -8,8 +9,7 @@ namespace SignalRChat
         public void Configuration(IAppBuilder app)
         {
             // Any connection or hub wire up and configuration should go here
-            RouteTable.Routes.MapHubs();
-            app.MapSignalR();
+            app.MapSignalR("/signalr", new HubConfiguration());
         }
     }
 }
