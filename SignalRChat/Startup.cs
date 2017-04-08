16de23302dbae3af5c98289c34f9a7484de1792c
unknown <alumnos@H41-05.upc.edu.pe>
2017-04-08 18:43:38 -0500

    nuevo signalR

--- SignalRChat/Startup.cs
+++ SignalRChat/Startup.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
-using Microsoft.AspNet.SignalR;
+﻿using Owin;
+using Microsoft.Owin;
 [assembly: OwinStartup(typeof(SignalRChat.Startup))]
 namespace SignalRChat
 {
@@ -9,7 +8,7 @@ namespace SignalRChat
         public void Configuration(IAppBuilder app)
         {
             // Any connection or hub wire up and configuration should go here
-            app.MapSignalR("/signalr", new HubConfiguration());
+            app.MapSignalR();
         }
     }
 }
